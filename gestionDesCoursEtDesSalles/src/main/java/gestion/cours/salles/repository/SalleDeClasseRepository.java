4394c981bd9f088336bc892b6dfb0ef76235bc94
abdokorachi3 <162052616+abdokorachi3@users.noreply.github.com>
2024-11-10 02:51:18 +0100

    Add files via upload

---
+++ gestionDesCoursEtDesSalles/src/main/java/gestion/cours/salles/repository/SalleDeClasseRepository.java
@@ -0,0 +1,14 @@
+package gestion.cours.salles.repository;
+
+import gestion.cours.salles.entities.SalleDeClasse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SalleDeClasseRepository extends JpaRepository<SalleDeClasse, Long> {
+    Page<SalleDeClasse> findByNomContaining(String nom, Pageable pageable);
+}
